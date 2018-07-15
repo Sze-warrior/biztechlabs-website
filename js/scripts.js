1f60918c5bbf120d56322350b92434ad88afe774
Michael Szeto <szetomj@gmail.com>
2018-07-15 23:12:03 +1200

    Built mobile visibility and JS interactions
    Built mobile visibility and JS interactions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,48 @@
 "use strict";
 
+//Variables
 var btnLearnMore = document.querySelector(".button--learn-more");
 var section = document.querySelector(".test");
+var featuresScroller = document.querySelector(".features-wrapper");
+var industry = document.querySelector(".industry");
 
+//Function to autoscroll on hero banner
 btnLearnMore.addEventListener("click", function (event) {
 	Velocity(section, "scroll", { duration: 700, easing: "ease-in-out" });
+});
+
+//Function for mobile scroller
+featuresScroller.addEventListener("scroll", function (event) {
+
+	var leftPosition = featuresScroller.scrollLeft;
+	var circle = document.querySelectorAll(".circle");
+	var scrollLeftSecondCard = 210;
+	var scrollLeftThirdCard = 430;
+
+	if (leftPosition > 150) {
+
+		for (var i = 0; i < circle.length; i++) {
+			circle[i].classList.remove("circle--active");
+			circle[1].classList.add("circle--active");
+		}
+	} else {
+
+		for (var i = 0; i < circle.length; i++) {
+			circle[i].classList.remove("circle--active");
+			circle[0].classList.add("circle--active");
+		}
+	}
+
+	if (leftPosition > 380) {
+		for (var i = 0; i < circle.length; i++) {
+			circle[i].classList.remove("circle--active");
+			circle[2].classList.add("circle--active");
+		}
+		featuresScroller.scrollLeft = scrollLeftThirdCard;
+	} else if (leftPosition < 150) {
+		for (var i = 0; i < circle.length; i++) {
+			circle[i].classList.remove("circle--active");
+			circle[1].classList.add("circle--active");
+		}
+	}
 });
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,51 @@
+//Variables
 const btnLearnMore = document.querySelector(".button--learn-more");
 const section = document.querySelector(".test");
+const featuresScroller = document.querySelector(".features-wrapper");
+const industry = document.querySelector(".industry");
 
+//Function to autoscroll on hero banner
 btnLearnMore.addEventListener("click", (event) =>{
 	Velocity(section, "scroll", {duration: 700, easing: "ease-in-out"});
+});
+
+//Function for mobile scroller
+featuresScroller.addEventListener("scroll", (event) =>{
+
+	const leftPosition = featuresScroller.scrollLeft;
+	const circle = document.querySelectorAll(".circle");
+	const scrollLeftSecondCard = 210;
+	const scrollLeftThirdCard = 430;
+
+	if(leftPosition > 150){
+		
+		for(var i = 0; i < circle.length; i++){
+			circle[i].classList.remove("circle--active");
+			circle[1].classList.add("circle--active");
+		}
+
+	}else{
+		
+		for(var i = 0; i < circle.length; i++){
+			circle[i].classList.remove("circle--active");
+			circle[0].classList.add("circle--active");
+		}	
+
+	}
+
+
+	if(leftPosition > 380){
+		for(var i = 0; i < circle.length; i++){
+			circle[i].classList.remove("circle--active");
+			circle[2].classList.add("circle--active");
+		}
+		featuresScroller.scrollLeft = scrollLeftThirdCard;
+
+	}else if(leftPosition < 150){
+		for(var i = 0; i < circle.length; i++){
+			circle[i].classList.remove("circle--active");
+			circle[1].classList.add("circle--active");
+		}
+	}
+
 });
